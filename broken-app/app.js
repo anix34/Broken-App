3cbd1ff9ca6e1e3ecab1ed33ef08aff4a17e0951
anix34 <139282344+anix34@users.noreply.github.com>
2024-06-30 23:32:32 -0400

    Update app.js

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
-let axios = require('axios');
-let app = express(); // let instead of var.
+const axios = require('axios');
+const app = express();
 
-app.post('/', function(req, res, next) {
+app.use(express.json()); // To parse JSON bodies
+
+app.post('/', async function(req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      // Need pass d a value to find emplyee.
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+    let results = await Promise.all(req.body.developers.map(async d => {
+      let response = await axios.get(`https://api.github.com/users/${d}`);
+      return response.data;
+    }));
+
+    let out = results.map(r => ({ name: r.name, bio: r.bio }));
 
     return res.send(JSON.stringify(out));
-  } catch {
+  } catch (err) {
     next(err);
   }
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
